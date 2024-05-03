e47e4bd7daf2083756b2192e5a40ffcd7840ebbc
Abraham Asher <ashercsulb@gmail.com>
2024-05-03 09:57:10 -0700

    Added readme etc.

---
+++ README.txt
@@ -0,0 +1,11 @@
+1. node_modules directory
+The node_modules directory is not included in the solution.
+Run the following command to install the dependencies:
+  npm install
+
+2. .env file
+Modify the .env file with the connection to your own PostgreSQL database
+
+3. test.js
+This is not part of the solution.  
+It is sample code to demonstrate a function that will accept an Array or an Object.
--- test.js
+++ test.js
@@ -1,14 +1,20 @@
+// SAMPLE CODE
+// Not part of the solution
 
+// The function will accept an Array or an Object
+// If it's an array, it will display the array values
+// If it's an object, it will create an array from the object values and display it
 
 const inputArrayOrObject = (arrOrObj) => {
   if (arrOrObj instanceof Array) {
     console.log("Array values are: ", arrOrObj);
   } else {
-    const objValues = Object.values(arrOrObj);
-    console.log("Object values are: ", objValues);
+    const objValuesArr = Object.values(arrOrObj);
+    console.log("Object values are: ", objValuesArr);
   }
 }
 
+// Create an object and an array
 const myArray = [1, 2, 3, 4];
 
 const myObj = {
